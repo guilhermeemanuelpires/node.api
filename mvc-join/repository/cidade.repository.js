922213f3cf5b417ea744d130e3d558467d7b434e
Guilherme Pires <guilhermeemnuel45@gmail.com.br>
2019-09-01 14:22:22 -0300

    ajustando repository

--- mvc-join/repository/cidade.repository.js
+++ mvc-join/repository/cidade.repository.js
@@ -1,7 +1,7 @@
 const connection = require('../mysql-connection');
 
-const query = ' select c.id, c.nome as nome_cidade, c.estado_id, e.nome as nome_estado, e.sigla from cidade c ' + 
-              ' inner join estado e on e.id = c.estado_id ';
+const query = ' select c.id, c.nome as nome_cidade, c.estado_id, e.nome as nome_estado, e.sigla from cidade c ' +
+    ' inner join estado e on e.id = c.estado_id ';
 
 module.exports = {
     find: (callback) => {
@@ -15,10 +15,10 @@ module.exports = {
     },
     update: (params, callback) => {
         connection.query('update cidade set nome = ?, estado_id = ?  where id = ? ',
-                            [params.nome,
-                            params.estado_id,
-                            params.id
-                            ], callback);
+            [params.nome,
+            params.estado_id,
+            params.id
+            ], callback);
     },
     delete: (params, callback) => {
         connection.query('delete from cidade where id = ?', [params.id], callback);
